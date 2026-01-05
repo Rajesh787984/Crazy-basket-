7462042846f8c72ac32790a4b24f89e269536d22
Rajesh787984 <91108387+Rajesh787984@users.noreply.github.com>
2026-01-05 21:45:47 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  firebase: {
+  firebaseConfig: {
     apiKey: "AIzaSyA-IMOBdZK4NfcyfHShkM1h0UcfJ0pjom8",
     authDomain: "crazybasketmall.firebaseapp.com",
     projectId: "crazybasketmall",
-    storageBucket: "crazybasketmall.appspot.com",
+    storageBucket: "crazybasketmall.firebasestorage.app",
     messagingSenderId: "442095144764",
     appId: "1:442095144764:web:c5c9f6b6b5bc29f2169839",
     measurementId: "G-M4VPP5Q3KM"
