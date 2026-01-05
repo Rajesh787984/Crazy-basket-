f04434e28f8a5c8ca660e72984540326d737da5f
Rajesh787984 <91108387+Rajesh787984@users.noreply.github.com>
2026-01-05 21:54:22 +0530

    Update app.config.ts

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,10 +2,21 @@ import { ApplicationConfig, importProvidersFrom, provideZonelessChangeDetection 
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(FormsModule, ReactiveFormsModule),
     provideZonelessChangeDetection(),
     provideHttpClient(withFetch()),
+
+    provideFirebaseApp(() =>
+      initializeApp(environment.firebaseConfig)
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ]
 };
