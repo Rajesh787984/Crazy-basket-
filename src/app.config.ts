bb2b03de696ce8f2991dc3394427e1b92b5b00d5
Rajesh787984 <91108387+Rajesh787984@users.noreply.github.com>
2026-01-08 11:08:27 +0530

    Update app.config.ts

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,12 +1,20 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
-
-import { environment } from './environments/environment';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { routes } from './app.routes';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { environment } from './environments/environment';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(routes, withComponentInputBinding()),
     importProvidersFrom(FormsModule, ReactiveFormsModule),
     provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth())
   ]
 };
